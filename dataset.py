bb1a0f79f23a008358b9f4d8cd16d5e874689908
ns-rokuyon <ness3psi@gmail.com>
2019-01-01 18:20:06 +0900

    add dataset

--- dataset.py
+++ dataset.py
@@ -1,4 +1,5 @@
 import torch
+import h5py
 import numpy as np
 import albumentations as alb
 from PIL import Image
@@ -6,18 +7,97 @@ from torch.utils.data import Dataset
 from torchvision import transforms
 
 
-from data import (
-    name_label_dict,
-    n_class,
-    load_4ch_image,
-    load_4ch_image_train,
-    load_4ch_image_test,
-    load_4ch_image_ex,
-    Stats,
-    train_image_dir,
-    test_image_dir,
-    get_ex_image_full_db_supports
-)
+from data import *
+
+
+class HPAEnhancedDatasetMP(Dataset):
+    def __init__(self, df, size=(512, 512),
+                 use_augmentation=True,
+                 use_cutout=False, cutout_ratio=0.2):
+        self.df = df
+        self.size = size
+        self.use_augmentation = use_augmentation
+
+        self.ex_image_full_db_ids = get_ex_image_full_db_supports()
+
+        print(f'Size: {self.size}')
+
+        self.transformer = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(Stats.mean, Stats.std)
+        ])
+
+        self.resizer = alb.Resize(self.size[0], self.size[1], p=1)
+
+        if self.use_augmentation:
+            print('Augmentation is enabled')
+            self.augmentor = alb.Compose([
+                alb.HorizontalFlip(p=0.5),
+                alb.VerticalFlip(p=0.5),
+                alb.RandomRotate90(p=0.5),
+                alb.RandomBrightness(p=0.1),
+                alb.RandomContrast(p=0.1)
+            ])
+
+            self.hard_augmentor = alb.Compose([
+                alb.HorizontalFlip(p=0.5),
+                alb.VerticalFlip(p=0.5),
+                alb.RandomRotate90(p=0.5),
+                alb.OneOf([
+                    alb.IAAAdditiveGaussianNoise(p=0.5),
+                    alb.GaussNoise(p=0.5)
+                ], p=0.5),
+                alb.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.2, rotate_limit=45, p=0.5),
+                alb.OneOf([
+                    alb.OpticalDistortion(p=1),
+                    alb.GridDistortion(p=1),
+                ], p=0.5),
+                alb.RandomBrightness(p=0.5),
+                alb.RandomContrast(p=0.5)
+            ])
+
+            if use_cutout:
+                cutout_length = int(cutout_ratio * size[0])
+                self.transformer.transforms.append(Cutout(n_holes=1, length=cutout_length))
+                print(f'Append cutout to transformer (length: {cutout_length})')
+        else:
+            self.augmentor = None
+
+    def __len__(self):
+        return self.df.shape[0]
+
+    def __getitem__(self, i):
+        i = int(i)
+        row = self.df.iloc[i]
+
+        labels = [int(label) for label in row['Target'].split(' ')]
+        labels = np.eye(n_class, dtype=np.float32)[labels].sum(axis=0)
+
+        image_id = row['Id']
+        source = row['Source']
+
+        if source == 'train':
+            with h5py.File(str(images_h5_file), 'r') as image_db:
+                im = image_db[f'train/{image_id}'].value
+        elif source == 'ex':
+            if image_id in self.ex_image_full_db_ids:
+                with h5py.File(str(ex_full_images_h5_file), 'r') as ex_image_full_db:
+                    im = ex_image_full_db[f'ex/{image_id}'].value
+            else:
+                with h5py.File(str(ex_images_h5_file), 'r') as ex_image_db:
+                    im = ex_image_db[f'ex/{image_id}'].value
+
+        if self.use_augmentation:
+            if source == 'ex':
+                aug = self.hard_augmentor(image=im)
+            else:
+                aug = self.augmentor(image=im)
+            im = aug['image']
+
+        im = self.resizer.apply(im)
+        im = self.transformer(im)
+
+        return im, labels
 
 
 class HPAEnhancedDataset(Dataset):
